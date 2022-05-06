60a33d279c5782ce3ac160c16c6ea2d99a46f830
marzo20 <myjune91@gmail.com>
2022-05-06 13:55:37 -0700

    added boxes on canvas for Mario and hurdle

--- js/script.js
+++ js/script.js
@@ -3,3 +3,33 @@ const ctx = canvas.getContext('2d')
 
 canvas.width = window.innerWidth - 100
 canvas.height = window.innerHeight - 100
+
+// Make a square for Mario
+const Mario = {
+    x: 50,
+    y: 350,
+    width: 50,
+    height: 50,
+    draw(){
+        ctx.fillStyle = 'green'
+        ctx.fillRect (this.x, this.y, this.width, this.height)
+    }
+}
+Mario.draw()
+
+// Make a square for hurdle
+
+class Obstacles {
+    constructor(){
+        this.x = 750
+        this.y = 350
+        this.width = 50
+        this.height = 50
+    }
+    draw(){
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+const hurdle = new Obstacles()
+hurdle.draw()
