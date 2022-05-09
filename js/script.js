7ef986599a5e603475e0f8d5fa82dc35d87a0b7a
marzo20 <myjune91@gmail.com>
2022-05-09 12:34:22 -0700

    put images of mario and hurdles

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
 
+
 canvas.width = window.innerWidth - 100
 canvas.height = window.innerHeight - 100
-
+const mario1 = new Image()
+mario1.src = 'images/Mario_jumping_.png'
+const bg = new Image()
+bg.src = 'images/running-track-bg.png'
+function Background(){
+    this.x = 0, this.y = 0, this.width = canvas.width, this.height = canvas.height
+    this.render = function (){
+        ctx.drawImage(bg, this.x--,0)
+        if(this.x <= -499){
+            this.x = 0
+        }
+    }
+}
+const background = new Background()
 // Make a square for Mario
 const mario = {
     x: 50,
@@ -12,13 +26,15 @@ const mario = {
     height: 50,
     draw(){
         ctx.fillStyle = 'green'
-        ctx.fillRect (this.x, this.y, this.width, this.height)
+        // ctx.fillRect (this.x, this.y, this.width, this.height)
+        ctx.drawImage(mario1, this.x, this.y)
     }
 }
 // mario.draw()
 
 // Make a square for hurdle
-
+const img1 = new Image()
+img1.src = 'images/hurdle_mario.jpeg'
 class Obstacles {
     constructor(){
         this.x = 1200
@@ -29,6 +45,7 @@ class Obstacles {
     draw(){
         ctx.fillStyle = 'red'
         ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.drawImage(img1, this.x, this.y)
     }
 }
 const hurdle = new Obstacles()
@@ -50,14 +67,15 @@ function movingHurdle(){
     // more hurdles
     timer++
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    if (timer% 300 === 0){
+    background.render()
+    if (timer% 180 === 0){
         let hurdle = new Obstacles()
         hurdles.push(hurdle)
           
     }
     // drawing many hurdles using forEach method and hurdles array
     hurdles.forEach((hurdle)=>{
-        hurdle.x --
+        hurdle.x -=5
         hurdle.draw()
        
     })
@@ -81,16 +99,16 @@ function movingHurdle(){
     
 // if space bar pressed, mario jumps
     if(jumping == true){
-        mario.y--
+        mario.y-=3
         jumpTimer++
         // console.log('spacebar clicked')
         // console.log(jumpTimer)
     }
     // mario coming down 
-    if(jumpTimer> 120){
+    if(jumpTimer> 70){
         jumping = false
         if(mario.y <350){
-            mario.y++
+            mario.y+=3
             if(mario.y ==350){
                 jumpTimer = 0
             }
