73c0eb6a3bfa4c20bc62afc5bcd4c460039bd435
marzo20 <myjune91@gmail.com>
2022-05-09 15:00:38 -0700

    changed hurdle to shell

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ canvas.height = window.innerHeight - 100
 const mario1 = new Image()
 mario1.src = 'images/Mario_jumping_.png'
 const bg = new Image()
-bg.src = 'images/running-track-bg.png'
+bg.src = 'images/645984.jpeg'
 function Background(){
     this.x = 0, this.y = 0, this.width = canvas.width, this.height = canvas.height
     this.render = function (){
-        ctx.drawImage(bg, this.x--,0)
-        if(this.x <= -499){
+        ctx.drawImage(bg, this.x--,-800)
+        if(this.x <= -2000){
             this.x = 0
         }
     }
@@ -21,30 +21,30 @@ const background = new Background()
 // Make a square for Mario
 const mario = {
     x: 50,
-    y: 350,
+    y: 550,
     width: 50,
     height: 50,
     draw(){
         ctx.fillStyle = 'green'
-        // ctx.fillRect (this.x, this.y, this.width, this.height)
-        ctx.drawImage(mario1, this.x, this.y)
+        ctx.fillRect (this.x, this.y, this.width, this.height)
+        ctx.drawImage(mario1, this.x-50, this.y-75)
     }
 }
 // mario.draw()
 
 // Make a square for hurdle
 const img1 = new Image()
-img1.src = 'images/hurdle_mario.jpeg'
+img1.src = 'images/GreenShell1.png'
 class Obstacles {
     constructor(){
         this.x = 1200
-        this.y = 350
+        this.y = 550
         this.width = 50
         this.height = 50
     }
     draw(){
         ctx.fillStyle = 'red'
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        // ctx.fillRect(this.x, this.y, this.width, this.height)
         ctx.drawImage(img1, this.x, this.y)
     }
 }
@@ -68,7 +68,7 @@ function movingHurdle(){
     timer++
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     background.render()
-    if (timer% 180 === 0){
+    if (timer% 160 === 0){
         let hurdle = new Obstacles()
         hurdles.push(hurdle)
           
@@ -81,7 +81,7 @@ function movingHurdle(){
     })
     // check if mario hit each hurdles
     hurdles.forEach((hurdle, i, o) => {
-        if (hurdle.x <0){
+        if (hurdle.x <-5){
             o.splice(i, 1)
             // counting passed hurdles for record
           const status = document.querySelector('#status')
@@ -93,8 +93,9 @@ function movingHurdle(){
             
 
         }
-        collision(mario, hurdle)
+        // background.render()
         hurdle.draw()
+        collision(mario, hurdle)
     })
     
 // if space bar pressed, mario jumps
@@ -107,9 +108,9 @@ function movingHurdle(){
     // mario coming down 
     if(jumpTimer> 70){
         jumping = false
-        if(mario.y <350){
+        if(mario.y <550){
             mario.y+=3
-            if(mario.y ==350){
+            if(mario.y ==550){
                 jumpTimer = 0
             }
         }    
@@ -133,6 +134,7 @@ function collision(mario, hurdle){
         // if mario hits hurdle, animation stop
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         cancelAnimationFrame(animation)
+    
     }
 }
 // not working...
